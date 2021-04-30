cded9ae75abcbda9359507e6fe237b927e298c56
Justin Boyer <jboyer@lucidchart.com>
2021-04-30 08:32:45 -0700

    Add framework for all cc prefixes

--- tests.py
+++ tests.py
@@ -18,20 +18,38 @@ def generate_testcases(tests_to_generate=100):
   for i in range(tests_to_generate):
     expected = True
     # Random length
-      length = random.randint(13,17)
-
+    length = random.randint(13,17)
     # Generate password
     cc = gen_cc('', length)
+    # Get prefix type
+    prefix_list = [visa, mc1, mc2, amex]
+    prefix = random.choice(prefix_list)
+    # Generate prefix value
+    if (prefix == 'visa'):
+        pass
+        # test_visa()
+    elif (prefix = 'mc1'):
+        pass
+        # test_mc1()
+    elif (prefix = 'mc2'):
+        pass
+        # test_mc2()
+    elif (prefix = 'amex'):
+        pass
+        # test_amex()        
     # Build test function
     message = 'Test case: {}, Expected: {}, Result: {}'
     new_test = build_test_func(expected, cc, credit_card_validator, message)
     setattr(TestCase, 'test_{}'.format(cc), new_test)
 
 
+def get_prefix(valid = true):
+    
+
 def gen_cc(prefix, length):
     cc_number = prefix
     cc_number = cc_number + ''.join(random.choice(string.digits) for i in range(length - len(prefix)))
-
+    return cc_number
 
 if __name__ == '__main__':
     generate_testcases()
